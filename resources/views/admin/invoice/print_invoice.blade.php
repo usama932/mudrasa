863872f10dcb3cf65c7650c3a6d6db02e6ef2bad
usama932 <usama1517a@gmail.com>
2022-07-28 12:56:13 +0500

    push changes

--- resources/views/admin/invoice/print_invoice.blade.php
+++ resources/views/admin/invoice/print_invoice.blade.php
@@ -14,6 +14,106 @@
     <!-- StyleSheets  -->
     <link rel="stylesheet" href="./assets/css/dashlite.css?ver=2.9.1">
     <link id="skin-default" rel="stylesheet" href="./assets/css/theme.css?ver=2.9.1">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <style>
+        .register{
+    background: -webkit-linear-gradient(left, #3931af, #00c6ff);
+    margin-top: 3%;
+    padding: 3%;
+}
+.register-left{
+    text-align: center;
+    color: #fff;
+    margin-top: 4%;
+}
+.register-left input{
+    border: none;
+    border-radius: 1.5rem;
+    padding: 2%;
+    width: 60%;
+    background: #f8f9fa;
+    font-weight: bold;
+    color: #383d41;
+    margin-top: 30%;
+    margin-bottom: 3%;
+    cursor: pointer;
+}
+.register-right{
+    background: #f8f9fa;
+    border-top-left-radius: 10% 50%;
+    border-bottom-left-radius: 10% 50%;
+}
+.register-left img{
+    margin-top: 15%;
+    margin-bottom: 5%;
+    width: 25%;
+    -webkit-animation: mover 2s infinite  alternate;
+    animation: mover 1s infinite  alternate;
+}
+@-webkit-keyframes mover {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-20px); }
+}
+@keyframes mover {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-20px); }
+}
+.register-left p{
+    font-weight: lighter;
+    padding: 12%;
+    margin-top: -9%;
+}
+.register .register-form{
+    padding: 10%;
+    margin-top: 10%;
+}
+.btnRegister{
+    float: right;
+    margin-top: 10%;
+    border: none;
+    border-radius: 1.5rem;
+    padding: 2%;
+    background: #0062cc;
+    color: #fff;
+    font-weight: 600;
+    width: 50%;
+    cursor: pointer;
+}
+.register .nav-tabs{
+    margin-top: 3%;
+    border: none;
+    background: #0062cc;
+    border-radius: 1.5rem;
+    width: 28%;
+    float: right;
+}
+.register .nav-tabs .nav-link{
+    padding: 2%;
+    height: 34px;
+    font-weight: 600;
+    color: #fff;
+    border-top-right-radius: 1.5rem;
+    border-bottom-right-radius: 1.5rem;
+}
+.register .nav-tabs .nav-link:hover{
+    border: none;
+}
+.register .nav-tabs .nav-link.active{
+    width: 100px;
+    color: #0062cc;
+    border: 2px solid #0062cc;
+    border-top-left-radius: 1.5rem;
+    border-bottom-left-radius: 1.5rem;
+}
+.register-heading{
+    text-align: center;
+    margin-top: 8%;
+    margin-bottom: -15%;
+    color: #495057;
+}
+    </style>
 </head>
 
 <body class="bg-white" onload="printPromot()">
@@ -21,94 +121,120 @@
         <div class="invoice invoice-print">
             <div class="invoice-wrap">
                 <div class="invoice-brand text-center">
-                    <img src="{{ asset('assets/images/logo-dark.png')}}" srcset="{{ asset('assets/images/logo-dark2x.png') }} 2x" alt="">
+                    <img src="{{ asset('assets/images/logo1.jpg')}}" srcset="{{ asset('assets/images/logo-dark2x.png') }} 2x" alt="">
                 </div>
-                <div class="invoice-head">
-                    <div class="invoice-contact">
-                        <span class="overline-title">Invoice To</span>
-                        <div class="invoice-contact-info">
-                            <h4 class="title">Gregory Anderson</h4>
-                            <ul class="list-plain">
-                                <li><em class="icon ni ni-map-pin-fill fs-18px"></em><span>House #65, 4328 Marion Street<br>Newbury, VT 05051</span></li>
-                                <li><em class="icon ni ni-call-fill fs-14px"></em><span>+012 8764 556</span></li>
-                            </ul>
-                        </div>
+                <div class="container register">
+                    <div class="row">
+                       <div class="col-md-3 register-left">
+                          <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
+                          <h3>Welcome</h3>
+                          <p>You are 30 seconds away from earning your own money!</p>
+                          <input type="submit" name="" value="Login"/><br/>
+                       </div>
+                       <div class="col-md-9 register-right">
+                          <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+                             <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Employee</a>
+                             </li>
+                             <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Hirer</a>
+                             </li>
+                          </ul>
+                          <div class="tab-content" id="myTabContent">
+                             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                <h3 class="register-heading">Apply as a Employee</h3>
+                                <div class="row register-form">
+                                   <div class="col-md-6">
+                                      <div class="form-group">
+                                         <input type="text" class="form-control" placeholder="First Name *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="text" class="form-control" placeholder="Last Name *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="password" class="form-control" placeholder="Password *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="password" class="form-control"  placeholder="Confirm Password *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <div class="maxl">
+                                            <label class="radio inline">
+                                            <input type="radio" name="gender" value="male" checked>
+                                            <span> Male </span>
+                                            </label>
+                                            <label class="radio inline">
+                                            <input type="radio" name="gender" value="female">
+                                            <span>Female </span>
+                                            </label>
+                                         </div>
+                                      </div>
+                                   </div>
+                                   <div class="col-md-6">
+                                      <div class="form-group">
+                                         <input type="email" class="form-control" placeholder="Your Email *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Your Phone *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <select class="form-control">
+                                            <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
+                                            <option>What is your Birthdate?</option>
+                                            <option>What is Your old Phone Number</option>
+                                            <option>What is your Pet Name?</option>
+                                         </select>
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="text" class="form-control" placeholder="Enter Your Answer *" value="" />
+                                      </div>
+                                      <input type="submit" class="btnRegister"  value="Register"/>
+                                   </div>
+                                </div>
+                             </div>
+                             <div class="tab-pane fade show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <h3  class="register-heading">Apply as a Hirer</h3>
+                                <div class="row register-form">
+                                   <div class="col-md-6">
+                                      <div class="form-group">
+                                         <input type="text" class="form-control" placeholder="First Name *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="text" class="form-control" placeholder="Last Name *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="email" class="form-control" placeholder="Email *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="text" maxlength="10" minlength="10" class="form-control" placeholder="Phone *" value="" />
+                                      </div>
+                                   </div>
+                                   <div class="col-md-6">
+                                      <div class="form-group">
+                                         <input type="password" class="form-control" placeholder="Password *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="password" class="form-control" placeholder="Confirm Password *" value="" />
+                                      </div>
+                                      <div class="form-group">
+                                         <select class="form-control">
+                                            <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
+                                            <option>What is your Birthdate?</option>
+                                            <option>What is Your old Phone Number</option>
+                                            <option>What is your Pet Name?</option>
+                                         </select>
+                                      </div>
+                                      <div class="form-group">
+                                         <input type="text" class="form-control" placeholder="`Answer *" value="" />
+                                      </div>
+                                      <input type="submit" class="btnRegister"  value="Register"/>
+                                   </div>
+                                </div>
+                             </div>
+                          </div>
+                       </div>
                     </div>
-                    <div class="invoice-desc">
-                        <h3 class="title">Invoice</h3>
-                        <ul class="list-plain">
-                            <li class="invoice-id"><span>Invoice ID</span>:<span>66K5W3</span></li>
-                            <li class="invoice-date"><span>Date</span>:<span>26 Jan, 2020</span></li>
-                        </ul>
-                    </div>
-                </div><!-- .invoice-head -->
-                <div class="invoice-bills">
-                    <div class="table-responsive">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Item ID</th>
-                                    <th>Description</th>
-                                    <th>Price</th>
-                                    <th>Qty</th>
-                                    <th>Amount</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>24108054</td>
-                                    <td>Dashlite - Conceptual App Dashboard - Regular License</td>
-                                    <td>$40.00</td>
-                                    <td>5</td>
-                                    <td>$200.00</td>
-                                </tr>
-                                <tr>
-                                    <td>24108054</td>
-                                    <td>6 months premium support</td>
-                                    <td>$25.00</td>
-                                    <td>1</td>
-                                    <td>$25.00</td>
-                                </tr>
-                                <tr>
-                                    <td>23604094</td>
-                                    <td>Invest Management Dashboard - Regular License</td>
-                                    <td>$131.25</td>
-                                    <td>1</td>
-                                    <td>$131.25</td>
-                                </tr>
-                                <tr>
-                                    <td>23604094</td>
-                                    <td>6 months premium support</td>
-                                    <td>$78.75</td>
-                                    <td>1</td>
-                                    <td>$78.75</td>
-                                </tr>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <td colspan="2"></td>
-                                    <td colspan="2">Subtotal</td>
-                                    <td>$435.00</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2"></td>
-                                    <td colspan="2">Processing fee</td>
-                                    <td>$10.00</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2"></td>
-                                    <td colspan="2">TAX</td>
-                                    <td>$43.50</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2"></td>
-                                    <td colspan="2">Grand Total</td>
-                                    <td>$478.50</td>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div>
-                </div><!-- .invoice-bills -->
+                 </div>
             </div><!-- .invoice-wrap -->
         </div><!-- .invoice -->
     </div><!-- .nk-block -->
